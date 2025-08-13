0ee29d3afdd78f03ded0e6a5c2ce66f16746d059
tusharjain8465 <tusharjain8465@gmail.com>
2025-08-14 01:16:18 +0530

    Update WholesaleSalesBackendApplication.java

--- src/main/java/com/example/wholesalesalesbackend/WholesaleSalesBackendApplication.java
+++ src/main/java/com/example/wholesalesalesbackend/WholesaleSalesBackendApplication.java
@@ -6,12 +6,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 @SpringBootApplication(scanBasePackages = { "com.example.wholesalesalesbackend", "com.service" })
+@EnableScheduling
 public class WholesaleSalesBackendApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(WholesaleSalesBackendApplication.class, args);
 	}
@@ -23,4 +27,9 @@ public class WholesaleSalesBackendApplication {
 				.deserializers(new LocalDateTimeDeserializer(DateTimeFormatter.ISO_DATE_TIME));
 	}
 
+	// minutes
+	@Scheduled(cron = "0 */1 * * * *", zone = "Asia/Kolkata")
+	public void printHello() {
+		System.out.println("Hello");
+	}
 }
