49058d6cc90054fc175d2aee8270d49dab00ee7d
tusharjain8465 <tusharjain8465@gmail.com>
2025-08-14 01:23:52 +0530

    Create DepositService.java

---
+++ src/main/java/com/example/wholesalesalesbackend/service/DepositService.java
@@ -0,0 +1,36 @@
+package com.example.wholesalesalesbackend.service;
+
+import com.example.wholesalesalesbackend.dto.DepositUpdateRequest;
+import com.example.wholesalesalesbackend.model.Deposit;
+import com.example.wholesalesalesbackend.repository.DepositRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class DepositService {
+
+    @Autowired
+    private DepositRepository depositRepository;
+
+    public Deposit addDeposit(Deposit deposit) {
+        return depositRepository.save(deposit);
+    }
+
+    public List<Deposit> getDepositsByClientId() {
+        return depositRepository.findAll();
+    }
+
+    public Deposit updateDeposit(Long id, DepositUpdateRequest request) {
+        Deposit deposit = depositRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Deposit not found"));
+        deposit.setAmount(request.getAmount());
+        deposit.setNote(request.getNote());
+        return depositRepository.save(deposit);
+    }
+
+    public void deleteDeposit(Long id) {
+        depositRepository.deleteById(id);
+    }
+}
