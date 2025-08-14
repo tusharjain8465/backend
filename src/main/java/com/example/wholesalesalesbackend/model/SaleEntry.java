72e47f58bdcfc62a882a41d10c703eef3b2b29a3
tusharjain8465 <tusharjain8465@gmail.com>
2025-08-14 16:09:58 +0530

    Update SaleEntry.java

--- src/main/java/com/example/wholesalesalesbackend/model/SaleEntry.java
+++ src/main/java/com/example/wholesalesalesbackend/model/SaleEntry.java
@@ -13,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,6 +26,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "sale_entry")   // 👈 this is crucial
 public class SaleEntry {
 
     @Id
