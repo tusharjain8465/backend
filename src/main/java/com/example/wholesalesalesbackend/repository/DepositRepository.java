0bbc23e0f9b49ca97d1c66a3a9772973ce2614e5
tusharjain8465 <tusharjain8465@gmail.com>
2025-08-15 18:27:15 +0530

    Update DepositRepository.java

--- src/main/java/com/example/wholesalesalesbackend/repository/DepositRepository.java
+++ src/main/java/com/example/wholesalesalesbackend/repository/DepositRepository.java
@@ -2,10 +2,29 @@ package com.example.wholesalesalesbackend.repository;
 
 import com.example.wholesalesalesbackend.model.Deposit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface DepositRepository extends JpaRepository<Deposit, Long> {
     List<Deposit> findByClientId(Long clientId);
+
+    @Query(value = "SELECT t.* FROM public.deposits t " +
+            "WHERE t.client_id = :clientId AND DATE(t.deposit_date) BETWEEN :fromDate AND :toDate " +
+            "ORDER BY t.deposit_date", nativeQuery = true)
+    List<Deposit> findByClientIdAndDepositDateBetweenOrderByDepositDateDescCustom(
+            Long clientId,
+            @Param("fromDate") LocalDate fromDate,
+            @Param("toDate") LocalDate toDate);
+
+    @Query(value = "SELECT t.* FROM public.deposits t " +
+            "WHERE DATE(t.deposit_date) BETWEEN :fromDate AND :toDate " +
+            "ORDER BY t.deposit_date", nativeQuery = true)
+    List<Deposit> findByDepositDateBetweenOrderByDepositDateDescCustom(
+            @Param("fromDate") LocalDate fromDate,
+            @Param("toDate") LocalDate toDate);
 }
