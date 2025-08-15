1b65e3d43f67e1df9fd99f29e91caa9490d624e6
tusharjain8465 <tusharjain8465@gmail.com>
2025-08-16 00:32:09 +0530

    Update DepositRepository.java

--- src/main/java/com/example/wholesalesalesbackend/repository/DepositRepository.java
+++ src/main/java/com/example/wholesalesalesbackend/repository/DepositRepository.java
@@ -7,24 +7,32 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface DepositRepository extends JpaRepository<Deposit, Long> {
-    List<Deposit> findByClientId(Long clientId);
+        List<Deposit> findByClientId(Long clientId);
 
-    @Query(value = "SELECT t.* FROM public.deposits t " +
-            "WHERE t.client_id = :clientId AND DATE(t.deposit_date) BETWEEN :fromDate AND :toDate " +
-            "ORDER BY t.deposit_date", nativeQuery = true)
-    List<Deposit> findByClientIdAndDepositDateBetweenOrderByDepositDateDescCustom(
-            Long clientId,
-            @Param("fromDate") LocalDate fromDate,
-            @Param("toDate") LocalDate toDate);
+        @Query(value = "SELECT t.* FROM public.deposits t " +
+                        "WHERE t.client_id = :clientId AND DATE(t.deposit_date) BETWEEN :fromDate AND :toDate " +
+                        "ORDER BY t.deposit_date", nativeQuery = true)
+        List<Deposit> findByClientIdAndDepositDateBetweenOrderByDepositDateDescCustom(
+                        Long clientId,
+                        @Param("fromDate") LocalDate fromDate,
+                        @Param("toDate") LocalDate toDate);
 
-    @Query(value = "SELECT t.* FROM public.deposits t " +
-            "WHERE DATE(t.deposit_date) BETWEEN :fromDate AND :toDate " +
-            "ORDER BY t.deposit_date", nativeQuery = true)
-    List<Deposit> findByDepositDateBetweenOrderByDepositDateDescCustom(
-            @Param("fromDate") LocalDate fromDate,
-            @Param("toDate") LocalDate toDate);
+        @Query(value = "SELECT t.* FROM public.deposits t " +
+                        "WHERE DATE(t.deposit_date) BETWEEN :fromDate AND :toDate " +
+                        "ORDER BY t.deposit_date", nativeQuery = true)
+        List<Deposit> findByDepositDateBetweenOrderByDepositDateDescCustom(
+                        @Param("fromDate") LocalDate fromDate,
+                        @Param("toDate") LocalDate toDate);
+
+        @Query(value = "SELECT SUM(t.amount) FROM deposits t WHERE DATE(t.deposit_date) < :fromDate", nativeQuery = true)
+        Double getTotalDepositOfClient(@Param("fromDate") LocalDateTime fromDate);
+
+        @Query(value = "SELECT SUM(t.amount) FROM deposits t WHERE t.client_id = :clientId AND DATE(t.deposit_date) < :fromDate", nativeQuery = true)
+        Double getTotalDepositOfClient(@Param("clientId") Long clientId,
+                        @Param("fromDate") LocalDateTime fromDate);
 }
