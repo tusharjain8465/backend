cb50505298f88d1112850ecf5366611cf121514d
tusharjain8465 <tusharjain8465@gmail.com>
2025-08-16 13:59:03 +0530

    Update ClientRepository.java

--- src/main/java/com/example/wholesalesalesbackend/repository/ClientRepository.java
+++ src/main/java/com/example/wholesalesalesbackend/repository/ClientRepository.java
@@ -1,6 +1,7 @@
 package com.example.wholesalesalesbackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.wholesalesalesbackend.model.Client;
@@ -11,6 +12,10 @@ import java.util.Optional;
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
     Optional<Client> findByName(String name);
+
     boolean existsByName(String name);
+
+    @Query(value = "SELECT t.* FROM public.clients t ORDER BY t.name", nativeQuery = true)
+    List<Client> findAllOrderByNameDesc();
 
 }
